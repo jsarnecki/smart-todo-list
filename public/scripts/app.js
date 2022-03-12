15ee4055123682a3b75bc8b4d5cb49b8d30a9dbc
trijaychan <trijaychan@gmail.com>
2022-03-12 20:54:37 +0000

    fixed issue where category was not being returned

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,13 @@
 // Client facing scripts here
 
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'midterm'
+});
+
 const keywords = {
   watch: [],
   eat: ["Restaurant"],
@@ -7,41 +15,45 @@ const keywords = {
   buy: []
 };
 
-
 const APIKEY = 'AIzaSyBJ-eqdhdex18EpEdsIFsb-QwoeEauolF0';
 
-const checkCategory = (query) => {
-    const service_url = 'https://kgsearch.googleapis.com/v1/entities:search';
-    const  params = {
-      'query': query,
-      'limit': 5,
-      'indent': true,
-      key: APIKEY
-    };
+const fetchKnowledgeGraph = (query) => {
+  const url = `https://kgsearch.googleapis.com/v1/entities:search?query=${query}&limit=1&indent=true&key=${APIKEY}&_=1647056117597`
+  return $.get(url);
+};
 
-    $.getJSON(service_url + '?callback=?', params, function(response) {
-      const data = response.itemListElement[0].result;
+const checkCategory = (query) => {
+  return fetchKnowledgeGraph(query)
+    .then((res) => {
+      const data = res.itemListElement[0].result;
       const types = data["@type"];
-      console.log(types);
 
       for (const category in keywords) {
         for (const keyword of keywords[category]) {
           if (types.includes(keyword)) {
-            console.log(category);
             return category;
           }
         }
       }
 
-      console.log("not found");
-      return null;
-    });
+      return 'none';
+    })
 };
 
 
 $(document).ready(function() {
   $("#create-task").submit(function(event) {
     const query = $("#user-input").val();
+
     checkCategory(query)
+    .then((category) => {
+      const queryString = `
+      INSERT INTO tasks (user_id, description, category, date_created, is_complete)
+      VALUES ($1, $2, $3, $4, $5);
+      `;
+      const values = [1, query, category, new Date(), false];
+
+      pool.query(queryString, values);
+    })
   });
 })
