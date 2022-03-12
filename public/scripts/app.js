dc53a850e9d38e4b6c7985838ea450fee9820877
trijaychan <trijaychan@gmail.com>
2022-03-12 03:48:16 +0000

    created function that determines a category based on user input using google's knowledge graph search api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,47 @@
 // Client facing scripts here
+
+const keywords = {
+  watch: [],
+  eat: ["Restaurant"],
+  read: [],
+  buy: []
+};
+
+
+const APIKEY = 'AIzaSyBJ-eqdhdex18EpEdsIFsb-QwoeEauolF0';
+
+const checkCategory = (query) => {
+    const service_url = 'https://kgsearch.googleapis.com/v1/entities:search';
+    const  params = {
+      'query': query,
+      'limit': 5,
+      'indent': true,
+      key: APIKEY
+    };
+
+    $.getJSON(service_url + '?callback=?', params, function(response) {
+      const data = response.itemListElement[0].result;
+      const types = data["@type"];
+      console.log(types);
+
+      for (const category in keywords) {
+        for (const keyword of keywords[category]) {
+          if (types.includes(keyword)) {
+            console.log(category);
+            return category;
+          }
+        }
+      }
+
+      console.log("not found");
+      return null;
+    });
+};
+
+
+$(document).ready(function() {
+  $("#create-task").submit(function(event) {
+    const query = $("#user-input").val();
+    checkCategory(query)
+  });
+})
