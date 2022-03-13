ed790542bdb427d4cc9168252590c4e4aa157b20
Josh Sarnecki <joshsarnecki@gmail.com>
2022-03-13 04:02:35 +0000

    created functions: filter conjunctions, count how many categories are recognized.  Also added more words to the keyword in-memory db

--- public/scripts/input_logic/input_logic.js
+++ public/scripts/input_logic/input_logic.js
@@ -1,6 +1,6 @@
 
 const rp = require('request-promise-native');
-const APIKEY = require('/constants')
+const APIKEY = require('constants')
 //const request = require('request');
 
 let cmdInput = process.argv.slice(2);
@@ -14,8 +14,6 @@ const stringQueryTogether = function(arr) {
 
 const query = stringQueryTogether(cmdInput);
 console.log("input:", query);
-
-
 
 
 //Potentially add to promise chain if there are more api calls
@@ -36,26 +34,31 @@ console.log("input:", query);
 
 
 const keywords = {
-  watch: ["movie", "creativework"],
-  eat: ["restaurant", "organization", "corporation", "dish", "cuisine"],
-  read: ["book", "novel"],
-  buy: ["product", "productmodel"]
+  //Have cases for plurals
+  watch: ["movie", "creativework", "theatre", "cinema", "film", "tv", "television", "tvseason", "season", "premiered"],
+  eat: ["restaurant", "organization", "corporation", "dish", "cuisine", "dine"],
+  read: ["book", "novel", "fiction", "written", "author"],
+  buy: ["product", "productmodel", "shop"]
 };
 
 //if description includes 'film' / 'book/novel'
 
 //TODO
 //have the cmdInput string multiple words together with +
-//After the search, have the
+
 
 //if type brings back Thing, with no other/minimal different types, do another apicall /
 //or resend the query with some other search words
+//Maybe have a counter for how many keywords they markoff, and then return the keyword with the most
+
+
+
 //perhaps filter through a dictionary api, and only collect the words that are nouns
 //or perhaps instead have a database full of the most common verbs go/see/watch/etc which filters through
   //the original query to see if they exist, and potentially redirect like "read"-> books "watch"->movies "eat"->food
 
 const checkCategory = function(type) {
-  let resultArr = [];
+  let resultArr = ["type:", type];
   for (const category in keywords) {
     for (const keyword of keywords[category]) {
       if (type.toLowerCase().includes(keyword)) {
@@ -63,13 +66,47 @@ const checkCategory = function(type) {
         resultArr.push(category);
       }
     }
+    //console.log(resultArr);
+    //return resultArr;
   }
+
   if (!resultArr.length) {
     console.log(`Couldn't find matching types for ${type}`);
-   // return;
+    return;
   }
-  console.log(`${query} has these matching types: ${resultArr}`);
-  //return resultArr;
+  if (Array.isArray(resultArr)) {
+    for (const elm of resultArr) {
+      console.log(`${type} has this matching type: ${elm}`)
+    }
+  }
+  //console.log(`${query} has these matching types: ${resultArr}`);
+  return resultArr;
+}
+
+const countCategory = function(categoryArr) {
+  let counter = {
+    watch: 0,
+    eat: 0,
+    read: 0,
+    buy: 0
+  }
+
+  for (const cat of categoryArr) {
+    if (cat === 'watch') {
+      counter.watch++;
+    }
+    if (cat === 'eat') {
+      counter.eat++;
+    }
+    if (cat === 'read') {
+      counter.read++;
+    }
+    if (cat === 'buy') {
+      counter.buy++;
+    }
+  }
+
+  return counter;
 }
 
 // const getWordType = function(word) {
@@ -83,6 +120,19 @@ const checkCategory = function(type) {
 // }
 
 // getWordType('creative');
+
+const conjunctions = ["the", "a", "an", "and", "or", "that", "as", "than", "so", "since", "is", "of", "are", "in"];
+
+const removeConjunctions = function(arr) {
+  const freshArr = [];
+  for (const elm of arr) {
+    if (!conjunctions.includes(elm.toLowerCase())) {
+      freshArr.push(elm);
+    }
+  }
+  return freshArr;
+}
+
 
 
 const getData = function() {
@@ -102,9 +152,9 @@ const getData = function() {
     console.log("type", types);
     const newData = result.itemListElement[0].result;
 
-    for (let elm of types) {
-      checkCategory(elm);
-    }
+    // for (let elm of types) {
+    //   checkCategory(elm);
+    // }
 
     const dataObject = {
       name: newData.name,
@@ -112,7 +162,25 @@ const getData = function() {
       description_body: newData.detailedDescription.articleBody
     }
 
-    console.log(dataObject);
+    //if the types doesn't match something right away, break the description into an array, and run a similar test
+    //check description first to see if it sets off any categories
+    //filter out the conjunctions..?
+    //Upon filtering conjunctions, if there are no obvious matches, potentially go thru and
+      //filter everything out but the nouns of the description and run the search again on what is remaining??
+
+    let descriptionArr = dataObject.description_body;
+    descriptionArr = descriptionArr.split(" ");
+    console.log("split:", descriptionArr);
+    let conjunctionFreeArr = removeConjunctions(descriptionArr);
+    for (let elm of conjunctionFreeArr) {
+      let conjuctTypeArr = checkCategory(elm);
+      console.log("conjuctType:", conjuctTypeArr);
+      let countObj = countCategory(conjuctTypeArr);
+      console.log("countObj:", countObj);
+    }
+    console.log("conj.removed:", conjunctionFreeArr);
+
+    //console.log(dataObject);
     return dataObject;
   })
   .catch(err => {
