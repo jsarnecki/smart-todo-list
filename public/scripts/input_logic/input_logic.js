fd6203ed72d82fa1ae18828829aed62c05f77a38
Josh Sarnecki <joshsarnecki@gmail.com>
2022-03-13 05:02:56 +0000

    Began bringing refactoring functions to use each other, and cleaned up code work area separating into sections

--- public/scripts/input_logic/input_logic.js
+++ public/scripts/input_logic/input_logic.js
@@ -16,23 +16,8 @@ const query = stringQueryTogether(cmdInput);
 console.log("input:", query);
 
 
-//Potentially add to promise chain if there are more api calls
-// const checkData = function(data) { //2nd promise?
-//     const result = JSON.parse(data);
-//     const type = result.itemListElement[0].result['@type'];
-//     console.log("type", type);
-//     const newData = result.itemListElement[0].result;
-//     const dataObject = {
-//       name: newData.name,
-//       description: newData.description,
-//       description_body: newData.detailedDescription.articleBody
-//     }
-//     console.log(dataObject);
-//     return dataObject;
-//   }
 
-
-
+////////KEYWORDS DATABASE
 const keywords = {
   //Have cases for plurals
   watch: ["movie", "creativework", "theatre", "cinema", "film", "tv", "television", "tvseason", "season", "premiered"],
@@ -41,21 +26,10 @@ const keywords = {
   buy: ["product", "productmodel", "shop"]
 };
 
-//if description includes 'film' / 'book/novel'
-
-//TODO
-//have the cmdInput string multiple words together with +
-
-
-//if type brings back Thing, with no other/minimal different types, do another apicall /
-//or resend the query with some other search words
-//Maybe have a counter for how many keywords they markoff, and then return the keyword with the most
 
 
 
-//perhaps filter through a dictionary api, and only collect the words that are nouns
-//or perhaps instead have a database full of the most common verbs go/see/watch/etc which filters through
-  //the original query to see if they exist, and potentially redirect like "read"-> books "watch"->movies "eat"->food
+  ////////////HELPER FUNCTIONS
 
 const checkCategory = function(type) {
   let resultArr = ["type:", type];
@@ -109,20 +83,40 @@ const countCategory = function(categoryArr) {
   return counter;
 }
 
-// const getWordType = function(word) {
-//   rp(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-//   .then(data => {
-//     const result = JSON.parse(data);
 
-//     console.log(result[0].meanings[0].partOfSpeech)
-//     //result[0].meanings[THIS CAN BE MORE THAN 1, SO POTENTIALLY PUSH MULTIPLES INTO ARRAY AND COMPARE THE MAIN ONES]
-//   })
-// }
+    /*
+      take in conjunction array to loop through
+      each elm, check the category(elm), which returns array
+      loop thru that array and countCategory(arr)
+      and add these numbers to a master countObj
+      return the obj, and a string stating each count for that original array + the original query
+    */
 
-// getWordType('creative');
+const returnCountObject = function(array) {
+  let masterCountObj = {
+    watch: 0,
+    eat: 0,
+    read: 0,
+    buy: 0
+  };
+
+  for (const elm of array) {
+    let secondArray = checkCategory(elm);
+    let countObj = countCategory(secondArray);
+    for (count in countObj) {
+     if (masterCountObj[count]) {//I think this is screwed up, make sure it's connecting to the object keys correctly
+       masterCountObj[count]++;
+     }
+    }
+  }
+
+  console.log(`Total from masterCount: watch: ${masterCountObj.watch}, eat: ${masterCountObj.eat}, read: ${masterCountObj.read}, buy: ${masterCountObj.buy}`);
+  return masterCountObj;
+}
+
+
 
 const conjunctions = ["the", "a", "an", "and", "or", "that", "as", "than", "so", "since", "is", "of", "are", "in"];
-
 const removeConjunctions = function(arr) {
   const freshArr = [];
   for (const elm of arr) {
@@ -133,6 +127,35 @@ const removeConjunctions = function(arr) {
   return freshArr;
 }
 
+////////////////////NOTES ------- WORKFLOW ------- THINGS TO WORK ON
+
+
+//TODO
+
+//if type brings back Thing, with no other/minimal different types, do another api-call /
+//or resend the query with some other search words
+//Maybe have a counter for how many keywords they markoff, and then return the keyword with the most
+
+
+//perhaps filter through a dictionary api, and only collect the words that are nouns
+//or perhaps instead have a database full of the most common verbs go/see/watch/etc which filters through
+  //the original query to see if they exist, and potentially redirect like "read"-> books "watch"->movies "eat"->food
+
+
+
+    //if the types doesn't match something right away, break the description into an array, and run a similar test
+    //check description first to see if it sets off any categories
+    //filter out the conjunctions..?
+    //Upon filtering conjunctions, if there are no obvious matches, potentially go thru and
+      //filter everything out but the nouns of the description and run the search again on what is remaining??
+
+
+    //If the type just comes back as 'Thing' we need to append something like "movie"/'book' to the original query to check if anything different comes up
+
+
+
+
+/////////////////MAIN LOGIC
 
 
 const getData = function() {
@@ -154,7 +177,13 @@ const getData = function() {
 
     // for (let elm of types) {
     //   checkCategory(elm);
+    //   //ADD FUNCTION HERE INSTEAD OF LOOP
+    //   //Checking all the categories, we can add all the objects together to return one sum of the final counts for each
+    //   //Create function to loop thru all the types
     // }
+
+    const totalTypeCountObject = returnCountObject(types);
+    console.log("total types count:", totalTypeCountObject);
 
     const dataObject = {
       name: newData.name,
@@ -162,23 +191,27 @@ const getData = function() {
       description_body: newData.detailedDescription.articleBody
     }
 
-    //if the types doesn't match something right away, break the description into an array, and run a similar test
-    //check description first to see if it sets off any categories
-    //filter out the conjunctions..?
-    //Upon filtering conjunctions, if there are no obvious matches, potentially go thru and
-      //filter everything out but the nouns of the description and run the search again on what is remaining??
-
+    //Break the data.description_body, each word as elm into array
     let descriptionArr = dataObject.description_body;
     descriptionArr = descriptionArr.split(" ");
-    console.log("split:", descriptionArr);
+    // console.log("split:", descriptionArr);   //Here is the array from split
     let conjunctionFreeArr = removeConjunctions(descriptionArr);
-    for (let elm of conjunctionFreeArr) {
-      let conjuctTypeArr = checkCategory(elm);
-      console.log("conjuctType:", conjuctTypeArr);
-      let countObj = countCategory(conjuctTypeArr);
-      console.log("countObj:", countObj);
-    }
-    console.log("conj.removed:", conjunctionFreeArr);
+    //Remove all the useless words from the description
+
+    // for (let elm of conjunctionFreeArr) {
+    //   //Loops through the conjunctionFree description to match against the keywords
+    //   let conjuctTypeArr = checkCategory(elm);
+    //   console.log("conjuctType:", conjuctTypeArr);
+    //   //The categories are put into an array, and then counted to see which comes up the most
+    //   let countObj = countCategory(conjuctTypeArr);
+    //   console.log("countObj:", countObj);//Returns an object holding the counts for all the keywords
+
+    // }
+    const totalDescCountObject = returnCountObject(conjunctionFreeArr);
+    console.log("from description:", totalDescCountObject);
+
+
+    //console.log("conj.removed:", conjunctionFreeArr);
 
     //console.log(dataObject);
     return dataObject;
@@ -189,44 +222,23 @@ const getData = function() {
 }
 
 
-//If the type just comes back as 'Thing' we need to append something like "movie"/'book' to the original query to check if anything different comes up
-
-
 getData();
+
+
+/////////////DICTIONARY WORD API
+
+// const getWordType = function(word) {
+//   rp(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+//   .then(data => {
+//     const result = JSON.parse(data);
+
+//     console.log(result[0].meanings[0].partOfSpeech)
+//     //result[0].meanings[THIS CAN BE MORE THAN 1, SO POTENTIALLY PUSH MULTIPLES INTO ARRAY AND COMPARE THE MAIN ONES]
+//   })
+// }
+
+// getWordType('creative');
 
 
 
                                   ///---------------EXTRA---------------///
-// const checkCategory = (query) => {
-//     const service_url = 'https://kgsearch.googleapis.com/v1/entities:search';
-//     const  params = {
-//       'query': query,
-//       'limit': 5,
-//       'indent': true,
-//       key: APIKEY
-//     };
-
-//     $.getJSON(service_url + '?callback=?', params, function(response) {
-//       const data = response.itemListElement[0].result;
-//       const types = data["@type"];
-//       console.log(types);
-
-//       for (const category in keywords) {
-//         for (const keyword of keywords[category]) {
-//           if (types.includes(keyword)) {
-//             console.log(category);
-//             return category;
-//           }
-//         }
-//       }
-
-//       console.log("not found");
-//       return null;
-//     });
-// };
-// $(document).ready(function() {
-//   $("#create-task").submit(function(event) {
-//     const query = $("#user-input").val();
-//     checkCategory(query)
-//   });
-// })
