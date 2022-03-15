94e2f8da263f8e7ec8b0a126d6689cde0eeb27ad
Josh Sarnecki <joshsarnecki@gmail.com>
2022-03-15 03:40:56 +0000

    Began users routes.  Connected the login form with a query request to the database where it checks validates the email and password, then sets a cookie from the user id with parser

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,37 @@
 
 const express = require('express');
 const router  = express.Router();
+const cookieParser = require('cookie-parser');
+router.use(cookieParser());
+
+/*
+id: 1,
+username: "katstratford",
+email: "katstratford@email.com",
+password: "1234"
+*/
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+  router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const query = `SELECT id FROM users WHERE email = $1 AND password = $2;`;
+    const params = [email, password];
+
+    db.query(query, params)
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
+        console.log(data.rows);
+        if (data.rows.length === 0) {
+          res.status(400).send('Error: Invalid user login');
+          return;
+        }
+        res.cookie('userId', data.rows[0].id);
+
+        //const data = JSON.parse(data);
+        // const users = data.rows;
+        // res.json({ users });
+
+        res.redirect("/");
       })
       .catch(err => {
         res
