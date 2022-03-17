23e9b1652c1265530c961b5f99eed65e5ff7551d
trijaychan <trijaychan@gmail.com>
2022-03-17 22:04:35 +0000

    updated for demo

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds here
 INSERT INTO users (username, email, password)
-VALUES ('example_1', 'user1@example.com', '1234'),
-('example_2', 'user2@example.com', '1234');
+VALUES ('phe', 'phe@email.com', '1234'),
+('tristan', 'tristan@email.com', '1234');
